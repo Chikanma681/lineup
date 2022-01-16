6ca1f381bdc563d340e414f7a5acd287893288d7
Chikanma <chikanmaonuegbu@gmail.com>
2022-01-15 19:39:43 -0700

    form

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,47 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
-const SignupForm = () => {
-  return (
-    <Form>
-      <FormGroup>
-        <Label for="email">Email</Label>
-        <Input type="email" name="email" id="email" placeholder="email" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="password">Password</Label>
-        <Input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="password"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="name">Store Name</Label>
-        <Input type="name" name="name" id="name" placeholder="Store Name" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="description">Store Description</Label>
-        <Input
-          type="description"
-          name="description"
-          id="description"
-          placeholder="Store Description"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="address">Store Address</Label>
-        <Input
-          type="address"
-          name="address"
-          id="address"
-          placeholder="Store Address"
-        />
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
-  );
-};
+class SignupForm extends Component {
+  state ={
+    email:"",
+    password:"",
+    name:"",
+    description:"",
+    address:""
+  }
+
+  handleInputChange = (e) => {
+    
+  }
+  render() {
+    return (
+      <Form>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input type="email" value={this} placeholder="email" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input
+            type="password"
+            value={this.state}
+            placeholder="password"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="name">Store Name</Label>
+          <Input type="name" value={} placeholder="Store Name" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="description">Store Description</Label>
+          <Input
+            type="description"
+            value={}
+            placeholder="Store Description"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="address">Store Address</Label>
+          <Input
+            type="address"
+            value={}
+            placeholder="Store Address"
+          />
+        </FormGroup>
+        <Button>Submit</Button>
+      </Form>
+    );
+  }
+}
 
 export default SignupForm;
