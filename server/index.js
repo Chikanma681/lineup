e56101b6791a403f7f1f8dd8e0747d8ddfba92bb
Luke Wismer <lukewismer77@gmail.com>
2022-01-15 23:15:53 -0700

    update/delete queue

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/user.model')
+const Customer = require('./models/customer.model')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const dotenv = require('dotenv')
@@ -69,6 +70,54 @@ app.post('/api/login', async (req, res) => {
 		return res.json({ status: 'ok', user: token })
 	} else {
 		return res.json({ status: 'error', user: false })
+	}
+})
+
+app.post('/api/insert_user', async (req, res) => {
+	console.log(req.body)
+	try{
+		var new_customer = {
+			name: req.body.name,
+			phone_number: req.body.phone_number,
+			text_messages_enabled: req.body.text_messages_enabled,
+			time_waiting: 0
+		}
+	
+		User.findOneAndUpdate(
+			{store_name: req.body.store_name}, {$push: {"dynamic.queue": new_customer}},
+			{ upsert: true, new: true},
+			function (error, success) {
+				if (error) {
+					console.log(errorr);
+				} else {
+					console.log(success);
+				}
+			}).clone()
+		
+			res.json({status: 'ok'})
+	} catch (err) {
+		res.json({status: 'error', error: 'Error Updating DB'})
+	}
+	
+})
+
+app.post('/api/delete_user', async (req, res) => {
+	try {
+		if (req.body.name){
+			await User.updateOne({ store_name: req.body.store_name}, {
+				$pull: {
+					"dynamic.queue": {"name": req.body.name, "phone_number": req.body.phone_number}
+				}
+			}).clone()
+		}
+		else {
+			await User.updateOne({store_name: req.body.store_name}, {
+				$pop: { "dynamic.queue": -1}
+			}).clone()
+		}
+		res.json({'status': 'ok'})
+	} catch (err) {
+		res.json({status: 'error', error: 'Error Deleting DB'})
 	}
 })
 
